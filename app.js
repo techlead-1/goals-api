046da048e7272540acfd63f8503ecb48cafe7363
techlead <adesanya1ademola@gmail.com>
2025-03-26 17:26:10 +0100

    error middleware use in app.js with some other middlewares

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import { PORT, NODE_ENV } from './config/env.js';
 import connectToDatabase from "./database/mongodb.js";
+import cookieParser from "cookie-parser";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the goals API')
