8c72643a099bb6f4727cf62c48cd97174736ca38
techlead <adesanya1ademola@gmail.com>
2025-03-27 17:38:34 +0100

    authentication routes updated with a method

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,16 +1,15 @@
 import express from 'express'
+import {signUp} from "../controllers/auth.controller.js";
 
 const authRouter = express.Router()
 
-authRouter.post('/sign-up', async (req, res) => {
-    res.send('Sign up')
-})
+authRouter.post('/sign-up', signUp)
 
 authRouter.post('/sign-in', async (req, res) => {
     res.send('Login')
 })
 
-authRouter.post('/sign-out', async (req, res) => {
+authRouter.delete('/sign-out', async (req, res) => {
     res.send('Logout')
 })
 
