feee9876bb84cf355d9975ee9d8e408e62ae2aad
techlead <adesanya1ademola@gmail.com>
2025-03-27 16:14:30 +0100

    used the routes in app js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import { PORT, NODE_ENV } from './config/env.js';
 import connectToDatabase from "./database/mongodb.js";
 import cookieParser from "cookie-parser";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
+import goalRouter from "./routes/goal.route.js";
+import milestoneRouter from "./routes/milestone.route.js";
 
 const app = express();
 
@@ -10,6 +14,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(errorMiddleware);
+
+app.use('/api/v1/auth', authRouter);
+app.use('api/v1/users', userRouter);
+app.use('/api/v1/goals', goalRouter);
+app.use('/api/v1/goals', milestoneRouter);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the goals API')
