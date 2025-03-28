89a2537cba05cdd2decdca036b2837ca5aa67638
techlead <adesanya1ademola@gmail.com>
2025-03-28 10:00:51 +0100

    added new validations to goals schema

---
+++ controllers/goals.controller.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import Goal from "../models/goal.model.js";
+
+export const createGoal = async (req, res, next) => {
+    const session = await mongoose.startSession();
+    await session.startTransaction();
+
+    try {
+        const allowedFields =  [ 'name', 'description', 'category', 'start_date', 'end_date', 'status' ]
+
+        let goalData = {}
+        for (let key of allowedFields) {
+            if (req.body[key] !== undefined) {
+                goalData[key] = req.body[key];
+            }
+        }
+
+        const goal = await Goal.create([goalData], {session})
+
+        await session.commitTransaction()
+        await session.endSession()
+
+        res.status(201).json({
+            success: true,
+            message: 'Goal created successfully.',
+            data: {
+                goal: goal[0],
+            }
+        });
+    } catch (e) {
+        await session.abortTransaction();
+        await session.endSession();
+        next(e)
+    }
+}
