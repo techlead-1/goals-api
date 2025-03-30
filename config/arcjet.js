7264a79cb9338c78c2cf5dd5bc8c4d7abd011895
techlead <adesanya1ademola@gmail.com>
2025-03-30 09:10:26 +0100

    modified bot protection

--- config/arcjet.js
+++ config/arcjet.js
@@ -11,6 +11,10 @@ const aj = arcjet({
             mode: "LIVE",
             allow: [
                 "CATEGORY:SEARCH_ENGINE",
+                "TOOL:POSTMAN",
+                "TOOL:CURL",
+                "CATEGORY:DEVELOPER", // general dev tools
+                "LIBRARY:FETCH" // modern fetch-based requests (like from frontend)
             ],        }),        tokenBucket({
             mode: "LIVE",
             refillRate: 5, // Refill 5 tokens per interval  
