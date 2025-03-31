9aee4a50798883378f08de00de7b83e8b7167dbe
techlead <adesanya1ademola@gmail.com>
2025-03-31 09:20:33 +0100

    redirect to docs

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/api/v1/goals', authMiddleware, milestoneRouter);
 app.set('trust proxy', true);
 
 app.get('/', (req, res) => {
-    res.redirect('https://github.com/techlead-1/goals-api-docs')
+    res.redirect('https://github.com/techlead-1/goals-api')
 })
 
 app.use((req, res, next) => {
